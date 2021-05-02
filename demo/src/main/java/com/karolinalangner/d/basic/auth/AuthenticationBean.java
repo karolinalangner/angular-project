9733c6d7299f7bc90336f4669ff1d60bd1891303
Karolina Langner <karolinalangner1@gmail.com>
2021-05-02 15:43:30 +0100

    Add files via upload

---
+++ demo/src/main/java/com/karolinalangner/d/basic/auth/AuthenticationBean.java
@@ -0,0 +1,28 @@
+package com.karolinalangner.d.basic.auth;
+
+public class AuthenticationBean {
+
+	private String message;
+
+	
+
+	public AuthenticationBean(String message) {
+		// TODO Auto-generated constructor stub
+		this.message = message;
+	}
+	
+	public String getMessage() {
+		return message;
+	}
+
+	public void setMessage(String message) {
+		this.message = message;
+	}
+
+	@Override
+	public String toString() {
+		return String.format("HelloWorldBean [message=%s]", message);
+	}
+	
+	
+}
